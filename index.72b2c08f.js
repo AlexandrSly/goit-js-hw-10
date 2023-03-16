cc1e867deefcc152f879ecb2f8b1abfbf7884c59
AlexandrSly <97891841+AlexandrSly@users.noreply.github.com>
2023-03-16 22:26:10 +0000

    Deploying to gh-pages from @ AlexandrSly/goit-js-hw-10@858d6216821dec50d1cb9604575699d7045e3889 🚀

--- index.72b2c08f.js
+++
@@ -1,2 +0,0 @@
-axios.create({baseURL:"https://dummyjson.com"});!async function(){const t={title:"I changed"};try{const o=await"https://restcountries.com/v3.1/".put("all/",t);console.log(o)}catch(t){console.log(t)}}();
-//# sourceMappingURL=index.72b2c08f.js.map
